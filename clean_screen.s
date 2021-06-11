40d4c84b8371141098b388d38c257e265d0df8b4
AgustinAbo22 <84516778+AgustinAbo22@users.noreply.github.com>
2021-06-11 10:53:13 -0300

    Hola

--- clean_screen.s
+++ clean_screen.s
@@ -4,7 +4,7 @@ clean_screen:
 	li $t1,0
 	loop_clean_screen:
 		addiu $t1, $t1,1
-		beq $t1, 8192, end_clean_screen   # 32768/8 = 8192 word para completar la imagen
+		beq $t1, 8192, end_clean_screen   # 32768/4 = 8192 word para completar la imagen
 		sw $zero, ($t0)
 		addiu $t0, $t0, 4
 		j loop_clean_screen
